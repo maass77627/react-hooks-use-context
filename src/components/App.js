0ce44f277b2e3dc66232ae87b42688d964e0956a
Kelly Maass <kmaass77627@gmail.com>
2026-01-19 13:11:32 -0600

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,28 @@
 
 import Header from "./Header";
 import Profile from "./Profile";
- import { UserProvider } from "../context/user";
- import { ThemeProvider } from "../context/theme";
-//  import { useContext } from "react"
-  // import { ThemeContext } from "../context/theme"
+import { UserProvider } from "../context/user";
+import { ThemeProvider, ThemeContext } from "../context/theme";
+import { useContext } from "react";
+
+function AppContent() {
+  const { theme } = useContext(ThemeContext);
+
+  return (
+    <main className={theme}>
+      <Header />
+      <Profile />
+    </main>
+  );
+}
 
 function App() {
-  // const { theme } = useContext(ThemeContext);
-  // const [user, setUser] = useState(null);
   return (
-    <main   >
-       <ThemeProvider> 
-      <UserProvider> 
-      <Header   />
-      <Profile   />
-       </UserProvider> 
-      </ThemeProvider> 
-    </main>
+    <ThemeProvider>
+      <UserProvider>
+        <AppContent />
+      </UserProvider>
+    </ThemeProvider>
   );
 }
 
